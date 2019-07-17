a6dcb44023779f85ca62a1e9c5a835d2fadcb915
thatzenbuhler <t.hatzenbuhler@gmail.com>
2019-07-17 10:27:43 -0700

    Handle offline status

--- lib/main.dart
+++ lib/main.dart
@@ -11,24 +11,28 @@ import 'package:http/http.dart' as http;
 
 // Retrieve exchange rate data from API
 Future<CurrencyData> fetchPost() async {
-  final response =
-      await http.get("https://api.exchangeratesapi.io/latest?base=USD");
-
-  if (response.statusCode == 200) {
-    return CurrencyData.fromJson(json.decode(response.body));
-  } else {
-    return CurrencyData(rates: Rates(usd: 1.0, eur: 0.8910, czk: 22.79, huf: 290.34), date: '2019-07-16 (Offline)');
+  try {
+    final response =
+        await http.get("https://api.exchangeratesapi.io/latest?base=USD");
+    if (response.statusCode == 200) {
+      return CurrencyData.fromJson(json.decode(response.body));
+    } else {
+      throw Exception; // Default to offline data below
+    }
+  } catch (e) {
+    return Future<CurrencyData>.value(CurrencyData(
+        rates: Rates(usd: 1.0, eur: 0.8910, czk: 22.79, huf: 290.34),
+        date: '2019-07-16 (Offline)'));
   }
 }
 
 void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown
-    ]);
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
     return MaterialApp(
       title: "Currency Tracker",
       home: HomePage(rateData: fetchPost()),
