ee26cdd023b37ad84535718117e55f6fc2bd2e18
thatzenbuhler <t.hatzenbuhler@gmail.com>
2020-02-19 20:50:40 -0800

    Variable formatting

--- lib/currencydata.dart
+++ lib/currencydata.dart
@@ -27,146 +27,146 @@ class CurrencyData {
 }
 
 class Rates {
-  double cAD;
-  double hKD;
-  double iSK;
-  double pHP;
-  double dKK;
+  double cad;
+  double hkd;
+  double isk;
+  double php;
+  double dkk;
   double huf;
   double czk;
-  double gBP;
-  double rON;
-  double sEK;
-  double iDR;
-  double iNR;
-  double bRL;
-  double rUB;
-  double hRK;
-  double jPY;
-  double tHB;
-  double cHF;
+  double gbp;
+  double ron;
+  double sek;
+  double idr;
+  double inr;
+  double brl;
+  double rub;
+  double hrk;
+  double jpy;
+  double thb;
+  double chf;
   double eur;
-  double mYR;
-  double bGN;
-  double tRY;
-  double cNY;
-  double nOK;
-  double nZD;
-  double zAR;
+  double myr;
+  double bgn;
+  double trY;
+  double cny;
+  double nok;
+  double nzd;
+  double zar;
   double usd;
-  double mXN;
-  double sGD;
-  double aUD;
-  double iLS;
-  double kRW;
-  double pLN;
+  double mxn;
+  double sgd;
+  double aud;
+  double ils;
+  double krw;
+  double pln;
 
   Rates(
-      {this.cAD,
-      this.hKD,
-      this.iSK,
-      this.pHP,
-      this.dKK,
+      {this.cad,
+      this.hkd,
+      this.isk,
+      this.php,
+      this.dkk,
       this.huf,
       this.czk,
-      this.gBP,
-      this.rON,
-      this.sEK,
-      this.iDR,
-      this.iNR,
-      this.bRL,
-      this.rUB,
-      this.hRK,
-      this.jPY,
-      this.tHB,
-      this.cHF,
+      this.gbp,
+      this.ron,
+      this.sek,
+      this.idr,
+      this.inr,
+      this.brl,
+      this.rub,
+      this.hrk,
+      this.jpy,
+      this.thb,
+      this.chf,
       this.eur,
-      this.mYR,
-      this.bGN,
-      this.tRY,
-      this.cNY,
-      this.nOK,
-      this.nZD,
-      this.zAR,
+      this.myr,
+      this.bgn,
+      this.trY,
+      this.cny,
+      this.nok,
+      this.nzd,
+      this.zar,
       this.usd,
-      this.mXN,
-      this.sGD,
-      this.aUD,
-      this.iLS,
-      this.kRW,
-      this.pLN});
+      this.mxn,
+      this.sgd,
+      this.aud,
+      this.ils,
+      this.krw,
+      this.pln});
 
   Rates.fromJson(Map<String, dynamic> json) {
-    cAD = json['CAD'];
-    hKD = json['HKD'];
-    iSK = json['ISK'];
-    pHP = json['PHP'];
-    dKK = json['DKK'];
+    cad = json['CAD'];
+    hkd = json['HKD'];
+    isk = json['ISK'];
+    php = json['PHP'];
+    dkk = json['DKK'];
     huf = json['HUF'];
     czk = json['CZK'];
-    gBP = json['GBP'];
-    rON = json['RON'];
-    sEK = json['SEK'];
-    iDR = json['IDR'];
-    iNR = json['INR'];
-    bRL = json['BRL'];
-    rUB = json['RUB'];
-    hRK = json['HRK'];
-    jPY = json['JPY'];
-    tHB = json['THB'];
-    cHF = json['CHF'];
+    gbp = json['GBP'];
+    ron = json['RON'];
+    sek = json['SEK'];
+    idr = json['IDR'];
+    inr = json['INR'];
+    brl = json['BRL'];
+    rub = json['RUB'];
+    hrk = json['HRK'];
+    jpy = json['JPY'];
+    thb = json['THB'];
+    chf = json['CHF'];
     eur = json['EUR'];
-    mYR = json['MYR'];
-    bGN = json['BGN'];
-    tRY = json['TRY'];
-    cNY = json['CNY'];
-    nOK = json['NOK'];
-    nZD = json['NZD'];
-    zAR = json['ZAR'];
+    myr = json['MYR'];
+    bgn = json['BGN'];
+    trY = json['TRY'];
+    cny = json['CNY'];
+    nok = json['NOK'];
+    nzd = json['NZD'];
+    zar = json['ZAR'];
     usd = json['USD'];
-    mXN = json['MXN'];
-    sGD = json['SGD'];
-    aUD = json['AUD'];
-    iLS = json['ILS'];
-    kRW = json['KRW'];
-    pLN = json['PLN'];
+    mxn = json['MXN'];
+    sgd = json['SGD'];
+    aud = json['AUD'];
+    ils = json['ILS'];
+    krw = json['KRW'];
+    pln = json['PLN'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['CAD'] = this.cAD;
-    data['HKD'] = this.hKD;
-    data['ISK'] = this.iSK;
-    data['PHP'] = this.pHP;
-    data['DKK'] = this.dKK;
+    data['CAD'] = this.cad;
+    data['HKD'] = this.hkd;
+    data['ISK'] = this.isk;
+    data['PHP'] = this.php;
+    data['DKK'] = this.dkk;
     data['HUF'] = this.huf;
     data['CZK'] = this.czk;
-    data['GBP'] = this.gBP;
-    data['RON'] = this.rON;
-    data['SEK'] = this.sEK;
-    data['IDR'] = this.iDR;
-    data['INR'] = this.iNR;
-    data['BRL'] = this.bRL;
-    data['RUB'] = this.rUB;
-    data['HRK'] = this.hRK;
-    data['JPY'] = this.jPY;
-    data['THB'] = this.tHB;
-    data['CHF'] = this.cHF;
+    data['GBP'] = this.gbp;
+    data['RON'] = this.ron;
+    data['SEK'] = this.sek;
+    data['IDR'] = this.idr;
+    data['INR'] = this.inr;
+    data['BRL'] = this.brl;
+    data['RUB'] = this.rub;
+    data['HRK'] = this.hrk;
+    data['JPY'] = this.jpy;
+    data['THB'] = this.thb;
+    data['CHF'] = this.chf;
     data['EUR'] = this.eur;
-    data['MYR'] = this.mYR;
-    data['BGN'] = this.bGN;
-    data['TRY'] = this.tRY;
-    data['CNY'] = this.cNY;
-    data['NOK'] = this.nOK;
-    data['NZD'] = this.nZD;
-    data['ZAR'] = this.zAR;
+    data['MYR'] = this.myr;
+    data['BGN'] = this.bgn;
+    data['TRY'] = this.trY;
+    data['CNY'] = this.cny;
+    data['NOK'] = this.nok;
+    data['NZD'] = this.nzd;
+    data['ZAR'] = this.zar;
     data['USD'] = this.usd;
-    data['MXN'] = this.mXN;
-    data['SGD'] = this.sGD;
-    data['AUD'] = this.aUD;
-    data['ILS'] = this.iLS;
-    data['KRW'] = this.kRW;
-    data['PLN'] = this.pLN;
+    data['MXN'] = this.mxn;
+    data['SGD'] = this.sgd;
+    data['AUD'] = this.aud;
+    data['ILS'] = this.ils;
+    data['KRW'] = this.krw;
+    data['PLN'] = this.pln;
     return data;
   }
 }
